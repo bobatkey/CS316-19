d6b415e09a68f6c6092e37b533b5068b746fe594
Robert Atkey <bob.atkey@gmail.com>
2019-10-04 14:39:11 +0100

    [ lectures ] Lecture 4 initial plan

---
+++ lectures/Lec04.hs
@@ -0,0 +1,36 @@
+module Lec04 where
+
+import Test.QuickCheck
+
+{-    LECTURE 04 : RECURSIVE FUNCTIONS II -}
+
+
+-- using an accumulator (fast vs slow reverse)
+
+
+
+-- quickcheck property
+
+
+
+-- filtering lists
+
+
+
+-- quickcheck property
+
+
+
+-- quicksort
+
+
+
+-- quickcheck property
+
+
+
+-- treesort
+
+
+
+-- quickcheck testing
